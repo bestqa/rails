4dd6db496771a431f985354f280f610a3f50f360
Javen Chen <jchen@voxeo.com>
2012-10-21 11:31:17 +0800

    new portal with ruby on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestqaOrg::Application.config.secret_token = '1929113d8119920ba9d46930851c0279400dcbcfbd413732464f43a3f0c167affd2a32bf2b29c32ac35246da9857dc236f5fd9e5d557f6cc97c7ddfb687020c2'
